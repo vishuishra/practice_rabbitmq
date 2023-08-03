5d9505444974734356b51576573753367fb8aeb9
Vishu Mishra <mishra.vishu99@gmail.com>
2023-08-03 10:07:16 +0530

    Implemented handling both get and post through one endpoint; added async handlers

--- api.py
+++ api.py
@@ -1,48 +1,65 @@
 # app.py
-from fastapi import FastAPI, HTTPException
-import pika,json
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, HTTPException, Request
+import aio_pika
+
 
 app = FastAPI()
 
-def publish_to_queue(queue_name: str, message: str):
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue=queue_name)
-    channel.basic_publish(exchange='',
-                          routing_key=queue_name,
-                          body=message)
-    connection.close()
+async def publish_to_queue(queue_name: str, message: str):
+    connection = await aio_pika.connect_robust("amqp://guest:guest@localhost/")
+    channel = await connection.channel()
+    await channel.declare_queue(queue_name)
+    await channel.default_exchange.publish(
+        aio_pika.Message(body=message.encode()),
+        routing_key=queue_name  # Use the queue_name as the routing_key
+    )
+    await connection.close()
 
-
-def get_processed_message_from_queue(queue_name: str):
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue=queue_name)
-    method_frame, header_frame, body = channel.basic_get(queue=queue_name, auto_ack=True)
-    connection.close()
-    if method_frame:
-        return body.decode()
-    return None
-
-@app.post("/publish_message/")
-def publish_message(message: str):
-    try:
-        publish_to_queue("input_queue", message)
-        return {"message": "Message published to input_queue"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@app.get("/processed_message/")
-def get_processed_message():
-    processed_message = get_processed_message_from_queue("output_queue")
-    if processed_message:
-        # Convert the processed message to a JSON format
-        # processed_message_json = json.dumps({"processed_message": processed_message})
-        print(processed_message)
-
-        # Use JSONResponse to return the JSON-formatted processed message
-        return JSONResponse(content={"processed_message": processed_message})
+async def get_processed_message_from_queue(queue_name: str):
+    connection = await aio_pika.connect_robust("amqp://guest:guest@localhost/")
+    channel = await connection.channel()
+    queue = await channel.declare_queue(queue_name)
+    message = await queue.get()
+    if message:
+        processed_message = message.body.decode()
+        await message.ack()  # Acknowledge the message to remove it from the queue
     else:
-        return {"message": "No processed messages available"}
+        processed_message = None
+    await connection.close()
+    return processed_message
+
+# @app.post("/process_message/")
+# async def process_message(message: str):
+#     try:
+#         await publish_to_queue("input_queue", message)
+#         return {"message": "Message sent for processing"}
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
+
+# @app.get("/processed_message/")
+# async def get_processed_message():
+#     processed_message = await get_processed_message_from_queue("output_queue")
+#     if processed_message:
+#         return {"processed_message": processed_message}
+#     else:
+#         return {"message": "No processed messages available"}
+
+
+@app.post("/messages/")
+@app.get("/messages/")
+async def get_and_display_messages(request:Request):
+    if request.method == 'POST':
+        try:
+            request_body  = await request.json()
+            message = request_body.get('message')
+            await publish_to_queue("input_queue", message)
+            return {"message": "Message sent for processing"}
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+        
+    elif request.method == 'GET':
+        processed_message = await get_processed_message_from_queue("output_queue")
+        if processed_message:
+            return {"processed_message": processed_message}
+        else:
+            return {"message": "No processed messages available"}
